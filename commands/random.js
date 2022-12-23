8a2e23d671e7f97c707a0885cd1f71f89e87ddca
Yamami1221 <anak2547@gmail.com>
2022-12-24 00:17:29 +0700

    add random command

---
+++ commands/random.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('random')
+		.setDescription('Random number generator')
+        .addIntergerOption(option =>
+            option.setName('min')
+                .setDescription('The minimum number')
+                .setRequired(true))
+        .addIntergerOption(option =>
+            option.setName('max')
+                .setDescription('The maximum number')
+                .setRequired(true)),
+	async execute(interaction) {
+        var min = interaction.options.getInteger('min');
+        var max = interaction.options.getInteger('max');
+        if (min > max) {
+            var temp = min;
+            min = max;
+            max = temp;
+        }
+		await interaction.reply(`The random number between ${min} and ${max} is ${Math.floor(Math.random() * max) + min}!`);
+	},
+};
