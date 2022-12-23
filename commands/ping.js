683248fb520191f4b5ce60496ceea8b7c335d4bb
Yamami1221 <anak2547@gmail.com>
2022-12-23 16:07:40 +0700

    first commit

---
+++ commands/ping.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply({ content: 'Pong!', ephemeral: true });
+		await wait(2000);
+		await interaction.editReply({ content: 'Pong! again (edited)', ephemeral: true });
+	},
+};
