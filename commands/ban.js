fb0ee14f16fa3c84ec1ee6eae97b880e079d3cfe
Yamami1221 <anak2547@gmail.com>
2022-12-25 20:09:10 +0700

    added ban command

---
+++ commands/ban.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ban')
+		.setDescription('Select a member and ban them.')
+		.addUserOption(option =>
+			option
+				.setName('target')
+				.setDescription('The member to ban')
+				.setRequired(true))
+		.addStringOption(option =>
+			option
+				.setName('reason')
+				.setDescription('The reason for banning'))
+		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+		.setDMPermission(false),
+    async execute(interaction) {
+        const target = interaction.options.getUser('target');
+        const reason = interaction.options.getString('reason') ?? 'No reason provided';
+
+        if (interaction.member.permissions.has('BAN_MEMBERS') === false) {
+            return interaction.reply('You do not have permission to use this command.');
+        }
+        if (target.id === interaction.user.id) {
+            return interaction.reply('You cannot ban yourself.');
+        }
+        
+        await interaction.reply(`Banning ${target.username} for reason: ${reason}`);
+        await interaction.guild.members.ban(target);
+    },
+};
