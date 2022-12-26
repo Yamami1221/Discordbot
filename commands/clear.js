d5802b3c3873f19d64b692a5b9126a7767a071ea
Yamami1221 <anak2547@gmail.com>
2022-12-26 20:18:06 +0700

    add clear command

---
+++ commands/clear.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('clear')
+		.setDescription('Deletes messages in a channel.')
+                .addIntegerOption(option => 
+                    option.setName('amount')
+                    .setDescription('The amount of messages to delete.')
+                    .setMinValue(1)
+                    .setMaxValue(100)
+                    .setRequired(true)),
+	async execute(interaction) {
+        await interaction.deferReply();
+        const amount = interaction.options.getInteger('amount');
+        const messages = await interaction.channel.messages.fetch({ limit: amount });
+        await interaction.channel.bulkDelete(messages);
+        await interaction.editReply(`Deleted ${amount} messages!`);
+	},
+};
