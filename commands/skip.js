bf9a00f2bfcf2ac7a54fc71f307ad63da95af5d6
Yamami1221 <anak2547@gmail.com>
2022-12-28 00:29:54 +0700

    skip command v1

--- commands/skip.js
+++ commands/skip.js
@@ -1,10 +1,20 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { globalqueue } = require('../index.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('skip')
         .setDescription('Skip the current song'),
     async execute(interaction) {
+        skipSong(interaction);
         await interaction.reply({ content: 'This command is not available', ephemeral: true });
     },
 };
+
+async function skipSong(interaction) {
+    var serverQueue = globalqueue.get(interaction.guild.id);
+    if (!serverQueue) {
+        return await interaction.reply({ content: 'There is no song that I could skip!', ephemeral: true });
+    }
+    serverQueue.connection.dispatcher.end();
+}
