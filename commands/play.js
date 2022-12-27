ea66fe71181e3387a671b8d36b714de296e83bcb
Yamami1221 <anak2547@gmail.com>
2022-12-27 14:16:39 +0700

    start write play command

--- commands/play.js
+++ commands/play.js
@@ -3,8 +3,14 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
-		.setDescription('paly a song'),
+		.setDescription('paly a song')
+		.addStringOption(option =>
+			option.setName('link')
+				.setDescription('The song you want to play')
+				.setRequired(true)),
 	async execute(interaction) {
+		var link = interaction.options.getString('link');
+		console.log(link);
 		await interaction.reply({ content: 'This command is not available', ephemeral: true });
 	},
 };
