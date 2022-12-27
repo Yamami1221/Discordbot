06cc6dac51003c7a81b295d17a6a0a9537e63f77
Yamami1221 <anak2547@gmail.com>
2022-12-27 13:55:12 +0700

    prepare to add embed message

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -4,7 +4,7 @@ module.exports = {
     name: Events.MessageCreate,
     async execute(message) {
         if (message.content === '<@975433690874257458>') {
-            message.reply(`What?`);
+            message.reply(`What?(embed message will be added soon)`);
         }
         if (message.author.bot) return;
         if (message.channel.type === 'DM') return;
