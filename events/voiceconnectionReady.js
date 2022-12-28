eb8fdd1c8d1feedcc6f89578a8fdf5e7a891e492
Yamami1221 <anak2547@gmail.com>
2022-12-28 20:55:25 +0700

    fix some bugs

--- events/voiceconnectionReady.js
+++ events/voiceconnectionReady.js
@@ -3,6 +3,6 @@ const { VoiceConnectionStatus } = require('@discordjs/voice');
 module.exports = {
 	name: VoiceConnectionStatus.Ready,
 	async execute(connection) {
-		console.log(`Voice connection ready in ${connection.channel.guild.name}!`);
+		console.log(`Voice connection ready in ${connection.channel}!`);
 	},
 };
