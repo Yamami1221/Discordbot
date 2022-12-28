938482deb6700779e017aa42b9d4176c709ade70
Yamami1221 <anak2547@gmail.com>
2022-12-28 20:32:24 +0700

    add voiceconnection events

---
+++ events/voiceconnectionDisconnect.js
@@ -0,0 +1,15 @@
+const { VoiceConnectionStatus, entersState } = require('@discordjs/voice');
+
+module.exports = {
+	name: VoiceConnectionStatus.Disconnected,
+	async execute(connection) {
+		try {
+			await Promise.race([
+				entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
+				entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
+			]);
+		} catch (error) {
+			connection.destroy();
+		}
+	},
+};
