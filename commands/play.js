040b82c588977a2e6347f685b8f272ba9138dd1d
Yamami1221 <anak2547@gmail.com>
2022-12-28 15:15:01 +0700

    remove deferReply from play command

--- commands/play.js
+++ commands/play.js
@@ -8,7 +8,7 @@ module.exports = {
 			option.setName('link')
 				.setDescription('The song you want to play')
 				.setRequired(true)),
-	async execute(interaction) {
-		interaction.deferReply();
+	async execute() {
+
 	},
 };
