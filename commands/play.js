94f0ff4b1485b89d268f91018bd4b7c2cf2a59a0
Yamami1221 <anak2547@gmail.com>
2022-12-28 14:07:01 +0700

    reset to before add play command

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,4 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, entersState, VoiceConnectionStatus } = require('@discordjs/voice');
-const ytdl = require('ytdl-core');
-const { globalqueue } = require('../index.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
