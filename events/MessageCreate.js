29aa9ba29bd85a0021074c787ae15d744050c73f
Yamami1221 <anak2547@gmail.com>
2022-12-28 19:07:28 +0700

    add response to ping

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -1,10 +1,14 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
 		if (message.content === '<@975433690874257458>') {
-			message.reply('What?(embed message will be added soon)');
+			const embed = new EmbedBuilder()
+				.setTitle('Help')
+				.setColor('#ff8400')
+				.setDescription('you can use `/help` to get help');
+			message.reply({ content: 'What?', embeds: [embed] });
 		}
 		if (message.author.bot) return;
 		if (message.channel.type === 'DM') return;
