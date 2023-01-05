2906094f5eaca1776115c31ca8f960ec5b5c0100
Yamami1221 <anak2547@gmail.com>
2023-01-05 17:58:52 +0700

    fix: random command

--- commands/random.js
+++ commands/random.js
@@ -1,26 +1,30 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('random')
-		.setDescription('Random number generator')
-		.addIntegerOption(option =>
-			option.setName('min')
-				.setDescription('The minimum number')
-				.setRequired(true))
-		.addIntegerOption(option =>
-			option.setName('max')
-				.setDescription('The maximum number')
-				.setRequired(true)),
-	async execute(interaction) {
-		let min = interaction.options.getInteger('min');
-		let max = interaction.options.getInteger('max');
-		if (min > max) {
-			const temp = min;
-			min = max;
-			max = temp;
-		}
-		const random = Math.floor(Math.random() * max) + min;
-		await interaction.reply(`The random number between ${min} and ${max} is ${random}!`);
-	},
+    data: new SlashCommandBuilder()
+        .setName('random')
+        .setDescription('Random number generator')
+        .addIntegerOption(option =>
+            option.setName('min')
+                .setDescription('The minimum number')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('max')
+                .setDescription('The maximum number')
+                .setRequired(true)),
+    async execute(interaction) {
+        let min = interaction.options.getInteger('min');
+        let max = interaction.options.getInteger('max');
+        if (min > max) {
+            const temp = min;
+            min = max;
+            max = temp;
+        }
+        const random = getRndInteger(min, max);
+        await interaction.reply(`The random number between ${min} and ${max} is ${random}!`);
+    },
 };
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
