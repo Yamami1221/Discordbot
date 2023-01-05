71d01a92cf5ebfed23f135331774c0b05e7ffd21
Yamami1221 <anak2547@gmail.com>
2023-01-05 16:20:06 +0700

    add console.log read message content

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -1,17 +1,19 @@
 const { Events, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-	name: Events.MessageCreate,
-	async execute(message) {
-		if (message.content === '<@975433690874257458>') {
-			const embed = new EmbedBuilder()
-				.setTitle('Help')
-				.setColor('#ff8400')
-				.setDescription('you can use `/help` to get help');
-			message.reply({ content: 'What?', embeds: [embed] });
-		}
-		if (message.author.bot) return;
-		if (message.channel.type === 'DM') return;
-		if (!message.content.startsWith(process.env.PREFIX)) return;
-	},
+    name: Events.MessageCreate,
+    async execute(message) {
+        if (message.content === '<@975433690874257458>') {
+            const embed = new EmbedBuilder()
+                .setTitle('Help')
+                .setColor('#ff8400')
+                .setDescription('you can use `/help` to get help');
+            message.reply({ content: 'What?', embeds: [embed] });
+        } else if (message.content.startsWith('<@975433690874257458>')) {
+            console.log(message.content);
+        }
+        if (message.author.bot) return;
+        if (message.channel.type === 'DM') return;
+        if (!message.content.startsWith(process.env.PREFIX)) return;
+    },
 };
