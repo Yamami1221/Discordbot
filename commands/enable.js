7f4892380bf6e4cbbd3ebff8dda637cfff8741ab
Yamami1221 <anak2547@gmail.com>
2023-01-05 15:51:10 +0700

    add function to write data to json file

--- commands/enable.js
+++ commands/enable.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const fs = require('fs');
 
 const { globalqueue } = require('../global.js');
 
@@ -13,10 +14,10 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
         .setDMPermission(false),
     async execute(interaction) {
+        await interaction.deferReply({ ephemeral: true });
         if (interaction.member.permissions.has('MANAGE_CHANNELS')) {
             const channel = interaction.options.getChannel('channel');
-            console.log(channel);
-            const serverqueue = globalqueue.get(interaction.guild.id);
+            const serverqueue = globalqueue?.get(interaction.guild.id);
             if (!serverqueue) {
                 const queueconstruct = {
                     textchannel: interaction.channel,
@@ -31,9 +32,35 @@ module.exports = {
                     shuffle: false,
                 };
                 globalqueue.set(interaction.guild.id, queueconstruct);
+                console.log(globalqueue);
+                const data = JSON.stringify(globalqueue, replacer);
+                console.log(data);
+                fs.writeFile('../data.json', data, function(err) {
+                    if (err) {
+                        console.log('There has been an error saving your configuration data.');
+                        console.log(err.message);
+                        return;
+                    }
+                    console.log('Configuration saved successfully.');
+                });
+                await interaction.editReply({ content: `Enabled ${channel} for music commands` });
+            } else {
+                await interaction.editReply({ content: 'This channel is already enabled' });
             }
         } else {
-            await interaction.reply({ content: 'You do not have permission to use this command', ephemeral: true });
+            await interaction.editReply({ content: 'You do not have permission to use this command', ephemeral: true });
         }
     },
 };
+
+function replacer(key, value) {
+    if (value instanceof Map) {
+        return {
+            dataType: 'Map',
+            // eslint-disable-next-line no-inline-comments
+            value: Array.from(value.entries()), // or with spread: value: [...value]
+        };
+    } else {
+        return value;
+    }
+}
