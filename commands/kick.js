953828c75d3a20945ac764699efe67d14272f3a6
Yamami1221 <anak2547@gmail.com>
2023-01-19 01:33:18 +0700

    add reason for kick

--- commands/kick.js
+++ commands/kick.js
@@ -1,26 +1,31 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('kick')
-		.setDescription('Select a member and kick them.')
-		.addUserOption(option =>
-			option
-				.setName('target')
-				.setDescription('The member to kick')
-				.setRequired(true))
-		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers | PermissionFlagsBits.KickMembers)
-		.setDMPermission(false),
-	async execute(interaction) {
-		const target = interaction.options.getUser('target');
+    data: new SlashCommandBuilder()
+        .setName('kick')
+        .setDescription('Select a member and kick them.')
+        .addUserOption(option =>
+            option
+                .setName('target')
+                .setDescription('The member to kick')
+                .setRequired(true))
+        .addStringOption(option =>
+            option
+                .setName('reason')
+                .setDescription('The reason for banning'))
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers | PermissionFlagsBits.KickMembers)
+        .setDMPermission(false),
+    async execute(interaction) {
+        const target = interaction.options.getUser('target');
+        const reason = interaction.options.getString('reason') ?? 'No reason provided';
 
-		if (interaction.member.permissions.has('KICK_MEMBERS') === false) {
-			return interaction.reply('You do not have permission to use this command.');
-		}
-		if (target.id === interaction.user.id) {
-			return interaction.reply('You cannot kick yourself.');
-		}
-		await interaction.reply(`Kicking ${target.username}`);
-		await interaction.guild.members.kick(target);
-	},
+        if (interaction.member.permissions.has('KICK_MEMBERS') === false) {
+            return interaction.reply('You do not have permission to use this command.');
+        }
+        if (target.id === interaction.user.id) {
+            return interaction.reply('You cannot kick yourself.');
+        }
+        await interaction.reply(`Kicking ${target.username} for reason: ${reason}`);
+        await interaction.guild.members.kick(target);
+    },
 };
