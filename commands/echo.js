1ee4890ee86d8753cd45ea33a05db0a844fc26b4
Yamami1221 <anak2547@gmail.com>
2023-01-19 00:49:23 +0700

    fix ephemeral bug

--- commands/echo.js
+++ commands/echo.js
@@ -1,19 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('echo')
-		.setDescription('Replies with your input!')
-		.addStringOption(option =>
-			option.setName('input')
-				.setDescription('The input to echo back')
-				.setRequired(true))
-		.addBooleanOption(option =>
-			option.setName('ephemeral')
-				.setDescription('Whether or not the echo should be ephemeral')),
-	async execute(interaction) {
-		const input = interaction.options.getString('input');
-		const ephemeral = interaction.options.getBoolean('ephemeral');
-		await interaction.reply({ content: input, ephemeral });
-	},
+    data: new SlashCommandBuilder()
+        .setName('echo')
+        .setDescription('Replies with your input!')
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('The input to echo back')
+                .setRequired(true))
+        .addBooleanOption(option =>
+            option.setName('ephemeral')
+                .setDescription('Whether or not the echo should be ephemeral')),
+    async execute(interaction) {
+        const input = interaction.options.getString('input');
+        const ephemeral = interaction.options.getBoolean('ephemeral');
+        await interaction.reply({ content: input, ephemeral: ephemeral });
+    },
 };
