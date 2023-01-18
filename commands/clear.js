ff1afc3676f98ddb6416abf2fd457a92600e376b
Yamami1221 <anak2547@gmail.com>
2023-01-18 21:53:52 +0700

    update error message

--- commands/clear.js
+++ commands/clear.js
@@ -1,20 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('clear')
-		.setDescription('Deletes messages in a channel.')
-		.addIntegerOption(option =>
-			option.setName('amount')
-				.setDescription('The amount of messages to delete.')
-				.setMinValue(1)
-				.setMaxValue(100)
-				.setRequired(true)),
-	async execute(interaction) {
-		await interaction.deferReply();
-		const amount = interaction.options.getInteger('amount');
-		const messages = await interaction.channel.messages.fetch({ limit: amount });
-		await interaction.channel.bulkDelete(messages);
-		await interaction.channel.send(`Deleted ${amount} messages!`);
-	},
+    data: new SlashCommandBuilder()
+        .setName('clear')
+        .setDescription('Deletes messages in a channel.')
+        .addIntegerOption(option =>
+            option.setName('amount')
+                .setDescription('The amount of messages to delete.')
+                .setMinValue(1)
+                .setMaxValue(100)
+                .setRequired(true)),
+    async execute(interaction) {
+        try {
+            await interaction.deferReply();
+            const amount = interaction.options.getInteger('amount');
+            const messages = await interaction.channel.messages.fetch({ limit: amount });
+            await interaction.channel.bulkDelete(messages);
+            await interaction.channel.send(`Deleted ${amount} messages!`);
+        } catch (error) {
+            await interaction.editReply({ content: `${error}`, ephemeral: true });
+        }
+    },
 };
