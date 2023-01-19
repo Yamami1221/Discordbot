5e290403841ced83ea601d2306bc91f37538830d
Yamami1221 <anak2547@gmail.com>
2023-01-19 16:47:39 +0700

    update np command to use new embeds

--- commands/np.js
+++ commands/np.js
@@ -14,13 +14,22 @@ module.exports = {
 async function nowplaying(interaction) {
     await interaction.deferReply();
     const serverqueue = globalqueue.get(interaction.guild.id);
-    const title = serverqueue.songs[0].title;
+    let embed = new EmbedBuilder()
+        .setTitle('Now Playing')
+        .setDescription('This server is not enabled for music commands!');
+    if (!serverqueue) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    const title = serverqueue?.songs[0]?.title;
     const connection = interaction.member.voice.channel;
-    if (!connection) return interaction.editReply({ content: 'You need to be in a voice channel to use this command!', ephemeral: true });
-    if (!title) return interaction.editReply({ content: 'There is no song that I could show!', ephemeral: true });
+    embed = new EmbedBuilder()
+        .setTitle('Now Playing')
+        .setDescription('You need to be in a voice channel to use this command!');
+    if (!connection) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    embed = new EmbedBuilder()
+        .setTitle('Now Playing')
+        .setDescription('There is no song in queue right now');
+    if (!title) return interaction.editReply({ embeds: [embed], ephemeral: true });
     const songembed = new EmbedBuilder()
         .setTitle('Now Playing')
-        .setDescription(`**${serverqueue.songs[0].title}**`)
-        .setColor('#ff8400');
+        .setDescription(`**${serverqueue.songs[0].title}**`);
     await interaction.editReply({ embeds: [songembed] });
 }
