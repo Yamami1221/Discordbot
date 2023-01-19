36721731e1afbc5e8d7a2f79e1eedcbd8852eb1b
Yamami1221 <anak2547@gmail.com>
2023-01-19 14:24:06 +0700

    updated stats command to show more info

--- commands/stats.js
+++ commands/stats.js
@@ -1,16 +1,22 @@
-const { SlashCommandBuilder, EmbedBuilder, Client } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('stats')
-		.setDescription('Shows the stats of the bot'),
-	async execute(interaction) {
-		const client = Client();
-		const embed = new EmbedBuilder()
-			.setTitle('Stats')
-			.setDescription(`Stats of the bot:\n\nServers: ${client.guilds.cache.size}\nUsers: ${client.users.cache.size}\nChannels: ${client.channels.cache.size}\n\n(These are the current stats, they may not be accurate.)`)
-			.setColor('#FF0000')
-			.setTimestamp();
-		await interaction.reply({ embeds: [embed] });
-	},
+    data: new SlashCommandBuilder()
+        .setName('stats')
+        .setDescription('Shows the stats of (sharding) the bot'),
+    async execute(interaction) {
+        const guildcount = await client.shard.fetchClientValues('guilds.cache.size');
+        const usercount = await client.shard.fetchClientValues('users.cache.size');
+        const channelcount = await client.shard.fetchClientValues('channels.cache.size');
+        try {
+            const embed = new EmbedBuilder()
+                .setTitle('Stats')
+                .setDescription(`Stats of the bot:\n\nServers: ${guildcount.reduce((acc, guildCount) => acc + guildCount, 0)}\nUsers: ${usercount.reduce((accc, userCount) => accc + userCount, 0)}\nChannels: ${channelcount.reduce((acccc, channelCount) => acccc + channelCount, 0)}\n\n(These are the current stats, they may not be accurate.)`);
+            return interaction.reply({ embeds: [embed] });
+        } catch (err) {
+            console.log(err);
+        }
+    },
 };
