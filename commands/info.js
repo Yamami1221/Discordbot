d71d7c849460833c9fbc506a44f0d58b24ef4e1e
Yamami1221 <anak2547@gmail.com>
2023-01-19 16:15:13 +0700

    update info command to reply with embed message

--- commands/info.js
+++ commands/info.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const moment = require('moment');
 
 module.exports = {
@@ -21,17 +21,40 @@ module.exports = {
         if (subcommand === 'user') {
             const user = interaction.options.getUser('target');
             if (!user) {
-                await interaction.reply({ content: `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.\nYour tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`, ephemeral: true });
+                const embed = new EmbedBuilder()
+                    .setTitle('Info')
+                    .setDescription('Info about a user')
+                    .addField('Username', interaction.user.username)
+                    .addField('Tag', interaction.user.tag)
+                    .addField('ID', interaction.user.id)
+                    .addField('Joined at', moment(interaction.member.joinedAt).format('MMMM Do YYYY, h:mm:ss a'))
+                    .setTimestamp();
+                await interaction.reply({ embeds: [embed], ephemeral: true });
             } else {
-                await interaction.reply({ content: `Username:${user.username}.\n${user.username} tag: ${user.tag}\n${user.username} id: ${user.id}`, ephemeral: true });
+                const embed = new EmbedBuilder()
+                    .setTitle('Info')
+                    .setDescription('Info about a user')
+                    .addField('Username', user.username)
+                    .addField('Tag', user.tag)
+                    .addField('ID', user.id)
+                    .setTimestamp();
+                await interaction.reply({ embeds: [embed], ephemeral: true });
             }
         } else if (subcommand === 'server') {
             const fetchedMembers = interaction.guild.members.fetch({ withPresences: true });
             const totalOnline = fetchedMembers.filter(member => member.presence?.status === 'online');
             const servertimedata = convert(interaction.guild.id);
-            await interaction.reply({ content: `Server name: ${interaction.guild.name}\nThere are currently ${totalOnline.size} members online in this guild!\nTotal members: ${interaction.guild.memberCount}\nBuild on:${servertimedata[0]}\nServer time zone:${servertimedata[1]}\nCreated ${servertimedata[2]}`, ephemeral: true });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            const embed = new EmbedBuilder()
+                .setTitle('Info')
+                .setDescription('Info about the server')
+                .addField('Server name', interaction.guild.name)
+                .addField('Total members', interaction.guild.memberCount)
+                .addField('Members online', totalOnline.size)
+                .addField('Build on', servertimedata[0])
+                .addField('Server time zone', servertimedata[1])
+                .addField('Created', servertimedata[2])
+                .setTimestamp();
+            await interaction.reply({ embeds: [embed], ephemeral: true });
         }
     },
 };
