4d322cbb9bbf8088e5e48b380d848f2f52daf20d
Yamami1221 <anak2547@gmail.com>
2023-01-19 18:22:43 +0700

    update remove to show song name that was removed

--- commands/remove.js
+++ commands/remove.js
@@ -1,26 +1,52 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 const { globalqueue } = require('../global.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('remove')
-		.setDescription('Removes a song from the queue')
-		.addIntegerOption(option =>
-			option.setName('position')
-				.setDescription('The position of the song in the queue')
-				.setRequired(true)),
-	async execute(interaction) {
-		remove(interaction);
-	},
+    data: new SlashCommandBuilder()
+        .setName('remove')
+        .setDescription('Removes a song from the queue')
+        .addIntegerOption(option =>
+            option.setName('position')
+                .setDescription('The position of the song in the queue')
+                .setRequired(true)),
+    async execute(interaction) {
+        remove(interaction);
+    },
 };
 
 async function remove(interaction) {
-	await interaction.deferReply();
-	const serverqueue = globalqueue.get(interaction.guild.id);
-	if (!serverqueue) return interaction.editReply({ content: 'There is no song that I could remove!', ephemeral: true });
-	const index = interaction.options.getInteger('position');
-	if (index > serverqueue.songs.length) return interaction.editReply({ content: 'There is no song at that index!', ephemeral: true });
-	serverqueue.songs.splice(index - 1, 1);
-	await interaction.editReply({ content: `Removed the song at index ${index}!` });
+    await interaction.deferReply();
+    const voicechannel = interaction.member.voice.channel;
+    let embed = new EmbedBuilder()
+        .setTitle('Remove')
+        .setDescription('You need to be in a voice channel to use this command!');
+    if (!voicechannel) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    const serverqueue = globalqueue.get(interaction.guild.id);
+    embed = new EmbedBuilder()
+        .setTitle('Remove')
+        .setDescription('This server is not enabled music commands!');
+    if (!serverqueue) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    let enabled = false;
+    for (let i = 0; i < serverqueue.textchannels.length; i++) {
+        if (serverqueue.textchannels[i] == interaction.channel.id) {
+            enabled = true;
+            break;
+        }
+    }
+    embed = new EmbedBuilder()
+        .setTitle('Remove')
+        .setDescription('This channel is not enabled music commands!');
+    if (!enabled) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    embed = new EmbedBuilder()
+        .setTitle('Remove')
+        .setDescription('There is nothing in the queue!');
+    if (!serverqueue.songs[0]) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    const index = interaction.options.getInteger('position');
+    embed = new EmbedBuilder()
+        .setTitle('Remove')
+        .setDescription('You need to provide a valid position!');
+    if (index > serverqueue.songs.length) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    await interaction.editReply({ content: `Removed the song name: ${serverqueue.songs[index - 1].title}!` });
+    serverqueue.songs.splice(index - 1, 1);
 }
