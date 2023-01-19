e6477403687ded11f42c7472ffc334587c3c9c08
Yamami1221 <anak2547@gmail.com>
2023-01-19 17:07:27 +0700

    update pause command to use new embeds

--- commands/pause.js
+++ commands/pause.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 const { globalqueue } = require('../global.js');
 
@@ -13,8 +13,28 @@ module.exports = {
 
 async function pauses(interaction) {
     await interaction.deferReply();
+    serverqueue.playing = false;
     const serverqueue = globalqueue.get(interaction.guild.id);
-    if (!serverqueue) return interaction.editReply({ content: 'There is no song that I could pause!', ephemeral: true });
+    let embed = new EmbedBuilder()
+        .setTitle('Pause')
+        .setDescription('This server is not enabled for music commands!');
+    if (!serverqueue) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    const connection = interaction.member.voice.channel;
+    embed = new EmbedBuilder()
+        .setTitle('Pause')
+        .setDescription('You need to be in a voice channel to use this command!');
+    if (!connection) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    embed = new EmbedBuilder()
+        .setTitle('Pause')
+        .setDescription('There is no song in queue right now');
+    if (!serverqueue.songs[0]) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    embed = new EmbedBuilder()
+        .setTitle('Pause')
+        .setDescription('The music is already paused!');
+    if (serverqueue.playing == false) return interaction.editReply({ embeds: [embed], ephemeral: true });
     serverqueue.resource.pause();
-    await interaction.editReply({ content: 'Paused the music!' });
+    embed = new EmbedBuilder()
+        .setTitle('Pause')
+        .setDescription('Paused the music!');
+    await interaction.editReply({ embeds: [embed] });
 }
