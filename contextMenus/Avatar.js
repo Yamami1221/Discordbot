e7941478fae90935cb50b3b19a5e3638d90193a6
Yamami1221 <anak2547@gmail.com>
2023-01-20 19:21:37 +0700

    add contextMenus name Avatar

---
+++ contextMenus/Avatar.js
@@ -0,0 +1,17 @@
+const { ContextMenuCommandBuilder, EmbedBuilder, ApplicationCommandType } = require('discord.js');
+
+module.exports = {
+    data: new ContextMenuCommandBuilder()
+        .setName('Avatar')
+        .setDescription('Get the avatar of a user')
+        .setType(ApplicationCommandType.User)
+        .setDMPermission(false),
+    async execute(interaction) {
+        await interaction.deferReply();
+        const user = interaction.targetUser;
+        const embed = new EmbedBuilder()
+            .setTitle(`${user.username}'s Avatar`)
+            .setImage(user.displayAvatarURL({ dynamic: true, size: 4096 }));
+        await interaction.editReply({ embeds: [embed] });
+    },
+};
