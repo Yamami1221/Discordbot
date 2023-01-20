c47414ddb120f90f4447f23f5e3512b00dfc5768
Yamami1221 <anak2547@gmail.com>
2023-01-21 03:26:10 +0700

    add skip context menu commands

---
+++ commands/SkipCon.js
@@ -0,0 +1,43 @@
+const { ContextMenuCommandBuilder, ApplicationCommandType, EmbedBuilder } = require('discord.js');
+
+const { globalqueue } = require('../global.js');
+
+module.exports = {
+    data: new ContextMenuCommandBuilder()
+        .setName('Skip')
+        .setType(ApplicationCommandType.Message),
+    async execute(interaction) {
+        skip(interaction);
+    },
+};
+
+async function skip(interaction) {
+    await interaction.deferReply();
+    const connection = interaction.member.voice.channel;
+    let embed = new EmbedBuilder()
+        .setTitle('Skip')
+        .setDescription('you need to be in a voice channel to use this command!');
+    if (!connection) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    const serverqueue = globalqueue.get(interaction.guild.id);
+    embed = new EmbedBuilder()
+        .setTitle('Skip')
+        .setDescription('This server is not enabled for music commands!');
+    if (!serverqueue) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    let enabled = false;
+    for (let i = 0; i < serverqueue.textchannel.length; i++) {
+        if (serverqueue.textchannel[i].id == interaction.channel.id) enabled = true;
+    }
+    embed = new EmbedBuilder()
+        .setTitle('Skip')
+        .setDescription('This channel is not enabled for music commands!');
+    if (!enabled) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    embed = new EmbedBuilder()
+        .setTitle('Skip')
+        .setDescription('There are no songs in the queue!');
+    if (!serverqueue.songs[0]) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    embed = new EmbedBuilder()
+        .setTitle('Skip')
+        .setDescription('Skipped the song!');
+    serverqueue.player.stop();
+    await interaction.editReply({ embeds: [embed] });
+}
