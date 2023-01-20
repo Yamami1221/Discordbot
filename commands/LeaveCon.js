7831622b024d7a9437dae317c5e381160091bee4
Yamami1221 <anak2547@gmail.com>
2023-01-21 03:23:51 +0700

    add leave command

---
+++ commands/LeaveCon.js
@@ -0,0 +1,39 @@
+const { ContextMenuCommandBuilder, ApplicationCommandType, EmbedBuilder } = require('discord.js');
+
+const { globalqueue } = require('../global.js');
+
+module.exports = {
+    data: new ContextMenuCommandBuilder()
+        .setName('Leave')
+        .setType(ApplicationCommandType.Message),
+    async execute(interaction) {
+        leave(interaction);
+    },
+};
+
+async function leave(interaction) {
+    await interaction.deferReply();
+    const connection = interaction.member.voice.channel;
+    let embed = new EmbedBuilder()
+        .setTitle('Leave')
+        .setDescription('You need to be in a voice channel to use this command!');
+    if (!connection) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    const serverqueue = globalqueue.get(interaction.guild.id);
+    embed = new EmbedBuilder()
+        .setTitle('Leave')
+        .setDescription('This server is not enabled for music commands!');
+    if (!serverqueue) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    let enabled = false;
+    for (let i = 0; i < serverqueue.channel.length; i++) {
+        if (serverqueue.channel[i].id == interaction.channel.id) enabled = true;
+    }
+    embed = new EmbedBuilder()
+        .setTitle('Leave')
+        .setDescription('This channel is not enabled for music commands!');
+    if (!enabled) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    serverqueue.songs = [];
+    serverqueue.player.stop();
+    serverqueue.connection.destroy();
+    serverqueue.playing = false;
+    await interaction.deleteReply();
+}
---
+++ commands/leave.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const { globalqueue } = require('../global.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('leave')
+        .setDescription('Stops the music and clears the queue'),
+    async execute(interaction) {
+        stop(interaction);
+    },
+};
+
+async function stop(interaction) {
+    await interaction.deferReply();
+    const connection = interaction.member.voice.channel;
+    let embed = new EmbedBuilder()
+        .setTitle('Leave')
+        .setDescription('You need to be in a voice channel to use this command!');
+    if (!connection) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    const serverqueue = globalqueue.get(interaction.guild.id);
+    embed = new EmbedBuilder()
+        .setTitle('Leave')
+        .setDescription('This server is not enabled for music commands!');
+    if (!serverqueue) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    let enabled = false;
+    for (let i = 0; i < serverqueue.channel.length; i++) {
+        if (serverqueue.channel[i].id == interaction.channel.id) enabled = true;
+    }
+    embed = new EmbedBuilder()
+        .setTitle('Leave')
+        .setDescription('This channel is not enabled for music commands!');
+    if (!enabled) return interaction.editReply({ embeds: [embed], ephemeral: true });
+    serverqueue.songs = [];
+    serverqueue.player.stop();
+    serverqueue.connection.destroy();
+    serverqueue.playing = false;
+    await interaction.deleteReply();
+}
