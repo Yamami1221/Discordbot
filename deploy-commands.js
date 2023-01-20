f2b3c0bb66693dbb807f2bd289899b9e7d055a0b
Yamami1221 <anak2547@gmail.com>
2023-01-20 21:32:06 +0700

    remove contextMenus uploader

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,14 +8,9 @@ const clientId = process.env.CLIENT_ID;
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const contextMenuFiles = fs.readdirSync('./contextMenus').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-}
-for (const file of contextMenuFiles) {
-    const command = require(`./contextMenus/${file}`);
     commands.push(command.data.toJSON());
 }
 
@@ -24,14 +19,14 @@ const rest = new REST({ version: '10' }).setToken(token);
 // start deploying
 (async () => {
     try {
-        console.log(`Started refreshing ${commands.length} application (/) commands and context munu commands.`);
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
         const data = await rest.put(
             Routes.applicationCommands(clientId),
             { body: commands },
         );
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands and context munu commands.`);
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
         console.error(error);
     }
