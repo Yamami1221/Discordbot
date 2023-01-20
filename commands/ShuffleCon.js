9888da67d78f8f6eb688e1feae32ec88c6c58d7b
Yamami1221 <anak2547@gmail.com>
2023-01-21 03:42:38 +0700

    update Shuffle context menu name

--- commands/ShuffleCon.js
+++ commands/ShuffleCon.js
@@ -4,7 +4,7 @@ const { globalqueue } = require('../global.js');
 
 module.exports = {
     data: new ContextMenuCommandBuilder()
-        .setName('shuffle')
+        .setName('Shuffle')
         .setType(ApplicationCommandType.Message),
     async execute(interaction) {
         shuffle(interaction);
