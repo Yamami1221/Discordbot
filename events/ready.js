c85c6ac5438aa36aa19c085f86bf79bfa2766dcc
Yamami1221 <anak2547@gmail.com>
2023-01-22 04:22:34 +0700

    update Presence

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,14 @@
 const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setPresence({
-			activities: [{ name: '/help', type: ActivityType.Playing }],
-			status: 'online',
-		});
-	},
+    name: Events.ClientReady,
+    once: true,
+    async execute(client) {
+        const guildcount = await client.shard.fetchClientValues('guilds.cache.size');
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+        client.user.setPresence({
+            activities: [{ name: `/help | ${guildcount.reduce((acc, guildCount) => acc + guildCount, 0)} Server`, type: ActivityType.Playing }],
+            status: 'online',
+        });
+    },
 };
