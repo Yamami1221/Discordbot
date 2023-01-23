b3fd10fbbdf4dc71791f65d6bd3e2269740a8b74
Yamami1221 <anak2547@gmail.com>
2023-01-23 16:26:55 +0700

    remove console.dir test method

--- events/voiceconnectionReady.js
+++ events/voiceconnectionReady.js
@@ -3,7 +3,6 @@ const { VoiceConnectionStatus } = require('@discordjs/voice');
 module.exports = {
     name: VoiceConnectionStatus.Ready,
     async execute(connection) {
-        console.dir(connection);
         const guildcount = await connection.shard.fetchClientValues('guilds.cache.size');
         console.log(`Voice connection is ready for ${connection.user.username}#${connection.user.discriminator} in ${guildcount.reduce((acc, guildCount) => acc + guildCount, 0)} guilds`);
     },
