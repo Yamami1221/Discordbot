43a63f70a297c65fbb26d60fb241b23842f8f41d
Yamami1221 <anak2547@gmail.com>
2023-01-27 01:34:15 +0700

    fix some indent

--- commands/avartar.js
+++ commands/avartar.js
@@ -6,8 +6,10 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('avatar')
         .setDescription('Get a user\'s avatar')
-        .addUserOption(option => option.setName('user')
-            .setDescription('The user\'s avatar to show')),
+        .addUserOption(option =>
+            option
+                .setName('user')
+                .setDescription('The user\'s avatar to show')),
     async execute(interaction) {
         await interaction.deferReply();
         const serverQueue = globalqueue.get(interaction.guildId) || undefined;
