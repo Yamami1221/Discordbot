8b4ce92a0903f4ec6a59eb63ffce709af1249f89
Yamami1221 <anak2547@gmail.com>
2023-01-27 02:12:58 +0700

    update chatbot.js to ready for start project

--- commands/chatbot.js
+++ commands/chatbot.js
@@ -9,11 +9,25 @@ module.exports = {
         .addSubcommand(subcommand =>
             subcommand
                 .setName('enable')
-                .setDescription('Enable the chat bot'))
+                .setDescription('Enable the chat bot in the current channel'))
         .addSubcommand(subcommand =>
             subcommand
                 .setName('disable')
-                .setDescription('Disable the chat bot')),
+                .setDescription('Disable the chat bot in the current channel'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('teach')
+                .setDescription('Teach the chat bot')
+                .addStringOption(option =>
+                    option
+                        .setName('text')
+                        .setDescription('The text to teach the chat bot')
+                        .setRequired(true))
+                .addStringOption(option =>
+                    option
+                        .setName('response')
+                        .setDescription('The response to the text(for multiple responses, separate them with a comma)')
+                        .setRequired(true))),
     async execute(interaction) {
         await interaction.deferReply();
         const serverQueue = globalqueue.get(interaction.guildId) || undefined;
@@ -28,16 +42,32 @@ module.exports = {
         }
         const subcommand = interaction.options.getSubcommand();
         if (subcommand === 'enable') {
-            const embed = new EmbedBuilder()
-                .setTitle('Chat Bot')
-                .setDescription('This feature is not yet available');
-            await interaction.editReply({ embeds: [embed], ephemeral: true });
-        }
-        else if (subcommand === 'disable') {
-            const embed = new EmbedBuilder()
-                .setTitle('Chat Bot')
-                .setDescription('This feature is not yet available');
-            await interaction.editReply({ embeds: [embed], ephemeral: true });
+            enableChatBot(interaction);
+        } else if (subcommand === 'disable') {
+            disableChatBot(interaction);
+        } else if (subcommand === 'teach') {
+            teachChatBot(interaction);
         }
     },
 };
+
+async function enableChatBot(interaction) {
+    const embed = new EmbedBuilder()
+        .setTitle('Chat Bot')
+        .setDescription('This feature is not yet available');
+    await interaction.editReply({ embeds: [embed], ephemeral: true });
+}
+
+async function disableChatBot(interaction) {
+    const embed = new EmbedBuilder()
+        .setTitle('Chat Bot')
+        .setDescription('This feature is not yet available');
+    await interaction.editReply({ embeds: [embed], ephemeral: true });
+}
+
+async function teachChatBot(interaction) {
+    const embed = new EmbedBuilder()
+        .setTitle('Chat Bot')
+        .setDescription('This feature is not yet available');
+    await interaction.editReply({ embeds: [embed], ephemeral: true });
+}
