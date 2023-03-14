63cb81e2ed1f4d046cea22da1a2002737aa42f05
Yamami1221 <anak2547@gmail.com>
2023-03-14 21:50:44 +0700

    update randomfood.js

--- commands/randomfood.js
+++ commands/randomfood.js
@@ -6,24 +6,13 @@ module.exports = {
         .setName('randomfood')
         .setDescription('Random food generator'),
     async execute(interaction) {
-        const foodarrayraw = fs.readFileSync('./data/food.txt', 'utf8');
-        const foodarray = foodarrayraw.split('\n');
-        for (let i = 0; i < foodarray.length; i++) {
-            foodarray[i] = foodarray[i].trim();
-        }
-        console.log(foodarray);
-        const jsonToWrite = JSON.stringify(foodarray);
-        console.log(jsonToWrite);
-        fs.writeFile('./data/fooddata.json', jsonToWrite, err => {
-            if (err) {
-                console.log('There has been an error saving your configuration data.');
-                console.log(err.message);
-                const errembed = new EmbedBuilder()
-                    .setTitle('Enable')
-                    .setDescription('There has been an error saving your configuration data.');
-                interaction.editReply({ embeds: [errembed] });
-                return;
-            }
-        });
+        await interaction.deferReply();
+        const foodarrayraw = fs.readFileSync('./data/fooddata.json');
+        const foodarray = JSON.parse(foodarrayraw);
+        const randomfood = foodarray[Math.floor(Math.random() * foodarray.length)];
+        const embed = new EmbedBuilder()
+            .setTitle('Random Food Generator')
+            .setDescription(`You should eat ${randomfood}`);
+        await interaction.editReply({ embeds: [embed] });
     },
 };
