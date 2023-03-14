b40f3717b30fcc4a4b18f03c29751496e20ad2d7
Yamami1221 <anak2547@gmail.com>
2023-03-14 22:07:39 +0700

    fix randomfood and randommeme

--- commands/randomfood.js
+++ commands/randomfood.js
@@ -1,12 +1,24 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 
+const { globaldata } = require('../data/global');
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('randomfood')
         .setDescription('Random food generator'),
     async execute(interaction) {
         await interaction.deferReply();
+        const serverData = globaldata.get(interaction.guildId) || undefined;
+        if (serverData?.veriChannel) {
+            if (interaction.channel.id === serverData.veriChannel.id) {
+                const embed = new EmbedBuilder()
+                    .setTitle('Verification')
+                    .setDescription('You cannot use this command in the verification channel');
+                await interaction.editReply({ embeds: [embed], ephemeral: true });
+                return;
+            }
+        }
         const foodarrayraw = fs.readFileSync('./data/fooddata.json');
         const foodarray = JSON.parse(foodarrayraw);
         const randomfood = foodarray[Math.floor(Math.random() * foodarray.length)];
--- commands/randommeme.js
+++ commands/randommeme.js
@@ -1,11 +1,29 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const memes = require('random-memes');
+
+const { globaldata } = require('../data/global');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('randommeme')
         .setDescription('Random meme generator'),
     async execute(interaction) {
-        console.log('randommeme command executed');
-        console.log(interaction);
+        await interaction.deferReply();
+        const serverData = globaldata.get(interaction.guildId) || undefined;
+        if (serverData?.veriChannel) {
+            if (interaction.channel.id === serverData.veriChannel.id) {
+                const embed = new EmbedBuilder()
+                    .setTitle('Verification')
+                    .setDescription('You cannot use this command in the verification channel');
+                await interaction.editReply({ embeds: [embed], ephemeral: true });
+                return;
+            }
+        }
+        const meme = await memes.random();
+        const embed = new EmbedBuilder()
+            .setTitle('Random Meme')
+            .setDescription(meme.caption)
+            .setImage(meme.image);
+        await interaction.editReply({ embeds: [embed] });
     },
 };
