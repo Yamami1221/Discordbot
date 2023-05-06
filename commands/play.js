215b2e28c5dcd4972f8d2be6189f40de0da27ac9
Yamami1221 <anak2547@gmail.com>
2023-05-06 14:30:32 +0700

    try to fix disconnect function in play.js

--- commands/play.js
+++ commands/play.js
@@ -387,7 +387,7 @@ async function playSong(interaction, song) {
     });
     serverdata.player.on(AudioPlayerStatus.AutoPaused, async () => {
         serverdata.timervar = setTimeout(async () => {
-            if (serverdata.connection.status != 'disconnected') await serverdata.connection.destroy();
+            if (serverdata.connection) await serverdata.connection.destroy();
             serverdata.connection = null;
             serverdata.resource = null;
             serverdata.player = null;
